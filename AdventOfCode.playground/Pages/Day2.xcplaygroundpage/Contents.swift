a43c55ed9cbc1779db7144183190cd9614f5c473
Eugen Pirogoff <eugenpirogoff@me.com>
2020-12-03 12:48:47 +0100

    add file input for day 2

--- AdventOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,1 +1,31 @@
+import Foundation
 
+struct Policy: Codable, CustomStringConvertible {
+	let lower: Int
+	let upper: Int
+	let character: String
+	let password: String
+
+	init?(json: [String: Any]) {
+		print(json)
+		guard let lower = json["lower"] as? Int, let upper = json["upper"] as? Int, let character = json["character"] as? String, let password = json["password"] as? String else {
+			return nil
+		}
+
+		self.lower = lower
+		self.upper = upper
+		self.character = character
+		self.password = password
+	}
+
+	var description: String {
+		return "\nLower: \(lower), Upper:\(upper), Character: \(character), Password:\(password)"
+	}
+}
+
+let filePath = Bundle.main.path(forResource:"input", ofType: "txt")
+let contentData = FileManager.default.contents(atPath: filePath!)
+let input = try! JSONDecoder().decode([Policy].self, from: contentData!)
+
+print("-------------------Part 1-------------------")
+print(input)
