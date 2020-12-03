5201dfc1e16eb6fae37c1ba92211cf6e7b9dfff4
Eugen Pirogoff <eugenpirogoff@me.com>
2020-12-03 21:19:15 +0100

    adding day 2 part 2

--- AdventOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -6,6 +6,16 @@ struct Policy: Codable, CustomStringConvertible {
 	let character: String
 	let password: String
 
+	var lowerCharacter: Character {
+		let lowerIndex = password.index(password.startIndex, offsetBy: lower-1)
+		return password[lowerIndex]
+	}
+
+	var upperCharacter: Character {
+		let upperIndex = password.index(password.startIndex, offsetBy: upper-1)
+		return password[upperIndex]
+	}
+
 	init?(json: [String: Any]) {
 		print(json)
 		guard let lower = json["lower"] as? Int, let upper = json["upper"] as? Int, let character = json["character"] as? String, let password = json["password"] as? String else {
@@ -33,6 +43,16 @@ struct Policy: Codable, CustomStringConvertible {
 		return false
 	}
 
+	func isValidForPosition() -> Bool {
+		let char = Character(character)
+
+		if (char == lowerCharacter || char == upperCharacter) && !(char == lowerCharacter && char == upperCharacter) {
+			return true
+		}
+
+		return false
+	}
+
 	var description: String {
 		return "\nLower: \(lower), Upper:\(upper), Character: \(character), Password:\(password)"
 	}
@@ -49,5 +69,13 @@ let result = input.map { Policy -> Bool in
 }.filter{ $0 == true }.count
 
 print("Valid Passwords: \(result)")
+
+print("-------------------Part 2-------------------")
+
+let resultForPositions = input.map { Policy -> Bool in
+	return Policy.isValidForPosition()
+}.filter{ $0 == true }.count
+
+print("Valid Passwords for Positions: \(resultForPositions)")
 
 
